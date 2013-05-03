ba76e3a4b5dc21206e463de1ca35d32781d08978
Mahibul Hasan Sohag <hyde.sohag@gmail.com>
2013-05-03 20:07:23 +0600

    working

---
+++ digital-download/class.digital-downloads.php
@@ -0,0 +1,109 @@
+<?php 
+
+/*
+ * this class will handle the digital downlads
+ * */
+
+class LinkedinDownload_control{
+	
+	static $liscense_key = null;
+	static $email = null;
+
+	//initialize
+	static function init(){
+		
+		//when download starts
+		add_action('edd_update_payment_status', array(get_class(), 'generate_license_key'), 10, 3);
+		add_action('edd_send_test_email', array(get_class(), 'generate_test_license_key'));
+		
+		//sending email with download links. Using this hook we are also sending a license key
+		//add_filter('edd_purchase_receipt', array(get_class(), 'send_liscense_key'), 10, 3);
+	}
+	
+	
+	//test generation liscense key
+	static function generate_test_license_key(){
+		return self::generate_license_key(null, null, null);
+	}
+	
+	
+	/*process download*/
+	static function generate_license_key($payment_id, $new_status, $old_status){
+		
+		
+			// Make sure we don't send a purchase receipt while editing a payment
+		if ( isset( $_POST['edd-action'] ) && $_POST['edd-action'] == 'edit_payment' )
+			return;
+	
+		// Check if the payment was already set to complete
+		if ( $old_status == 'publish' || $old_status == 'complete' )
+			return; // Make sure that payments are only completed once
+	
+		// Make sure the receipt is only sent when new status is complete
+		if ( $new_status != 'publish' && $new_status != 'complete' )
+			return;
+
+		
+		self::$email = edd_get_payment_user_email( $payment_id );
+			
+		self::create_a_liscense();
+		//hook to edit the email
+		add_action('edd_email_body_header', 'attach_liscense_key');
+		
+	}
+	
+	
+	
+	//@hook edd_purchase_receipt
+	static function attach_liscense_key(){
+		if(self::$liscense_key){
+			echo "<h2>LiscenceKey: " . self::$liscense_key . "</h2>";		
+		}
+	}
+	
+	//creat a liscense
+	static function create_a_liscense(){
+		$uniq_id = uniqid();
+		
+		//var_dump($uniq_id); die();
+
+		if(self::is_unique($uniq_id)){
+			self::$liscense_key = $uniq_id;
+			if(!self::insert_key()){
+				self::$liscense_key = null;
+			}
+		}
+		else {
+			return self::create_a_liscense();
+		}
+						
+	}
+	
+	
+	//unique key test
+	static function is_unique($key){
+		global $wpdb;
+		$tables = LinkedInAuthentication::get_tables();
+		extract($tables);
+		
+		global $wpdb;		
+		$id = $wpdb->get_var("SELECT ID FROM $a WHERE l_key = '$key'");
+		
+		return ($id) ? false : true;
+		
+	}
+	
+	
+	//insert a key
+	static function insert_key(){
+		global $wpdb;
+		$tables = LinkedInAuthentication::get_tables();
+		extract($tables);
+		
+		$wpdb->insert($a, array('l_key'=>self::$liscense_key, 'email'=>self::$email), array('%s', '%s'));
+		
+		return $wpdb->insert_id;
+	}
+	
+}
+
--- main.php
+++ main.php
@@ -6,14 +6,30 @@
  * Description: It controls the linkedin authentication
  * */
 
+
+//including the digital download 
+include dirname(__FILE__) . '/digital-download/class.digital-downloads.php';
+
+
 class LinkedInAuthentication{
 	function __construct(){
 		add_action('admin_menu', array($this, 'admin_menu'));
 		add_action('init', array($this, 'authenticate_linkedin_application'));
+		
+		
+		/*
+		 * table creation to hold the keys
+		 * 
+		 * */
+		register_activation_hook(__FILE__, array(get_class(), 'activate_the_plugin'));
+		
+		
+		//digital downolad
+		LinkedinDownload_control::init();
 	}	
 	
 	function admin_menu(){ 		
-		add_menu_page('linkedin groups authentication in wordpress', 'LinkedIn', 'manage_options', 'ln_auth', array($this, 'linkedin_authentication'));
+		add_menu_page('linkedin groups authentication in wordpress', 'Ln Api Keys', 'manage_options', 'ln_auth', array($this, 'linkedin_authentication'));
 	}
 	
   //menupage for groups
@@ -55,6 +71,43 @@ class LinkedInAuthentication{
 		}
 	}
 	
+	
+	/*
+	 * tabel creation
+	 * */
+	static function activate_the_plugin(){
+		$tables = self::get_tables();
+		extract($tables);
+		
+		$sql[] = "CREATE TABLE IF NOT EXISTS $a(
+			ID bigint unsigned AUTO_INCREMENT,
+			l_key varchar(50) NOT NULL,
+			email varchar(50) NOT NULL,
+			primary key(ID)
+		)";
+	
+		$sql[] = "CREATE TABLE IF NOT EXISTS $b(
+			ln_id bigint unsigned NOT NULL,
+			meta_key varchar(50) NOT NULL,
+			meta_value text NOT NULL
+		)";
+		
+		if(!function_exists('dbDelta')) :
+			include ABSPATH . 'wp-admin/includes/upgrade.php';
+		endif;
+		
+		foreach($sql as $s){
+			dbDelta($s);
+		}
+	}
+	
+	static function get_tables(){
+		global $wpdb;
+		return array(
+			'a' => $wpdb->prefix . 'ln_liscesnse',
+			'b' => $wpdb->prefix . 'ln_liscense_meta'
+		);
+	}
 
 	
 }
